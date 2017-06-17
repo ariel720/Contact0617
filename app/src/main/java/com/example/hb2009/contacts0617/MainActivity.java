ecd42274e73ae091a03a8fa631214cce0067dc51
parksoo0650 <blue4595@naver.com>
2017-06-17 12:27:08 +0900

    first commit

---
+++ app/src/main/java/com/example/hb2009/contacts0617/MainActivity.java
@@ -0,0 +1,154 @@
+package com.example.hb2009.contacts0617;
+
+import android.content.Context;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    static abstract class QueryFactory{
+        Context context;
+        public QueryFactory(Context context) {this.context = context;}
+        public abstract SQLiteDatabase getDB();
+    }
+
+    public final static String DB_NAME = "ariel.db";
+    public final static String MEMBER_TABLE = "Members";
+    public final static String MEMBER_SEQ = "seq";
+    public final static String MEMBER_NAME = "name";
+    public final static String MEMBER_PASSWORD = "password";
+    public final static String MEMBER_PHONE = "phone";
+    public final static String MEMBER_EMAIL = "email";
+    public final static String MEMBER_ADDRESS = "address";
+    public final static String MEMBER_PHOTO = "photo";
+
+    interface LoginService {public void login();}
+    static class Member{int seq;String name,password,email,phone,addr,photo;}
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        final Context context=MainActivity.this;
+        final MemberLogin mLogin = new MemberLogin(context);
+
+        findViewById(R.id.loginBtn).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                EditText inputID= (EditText) findViewById(R.id.inputID);
+                EditText inputPW= (EditText) findViewById(R.id.inputPW);
+                final String sID=inputID.getText().toString();
+                final String sPW=inputPW.getText().toString();
+
+                mySQLiteHelper helper = new mySQLiteHelper(context); //DB만들기
+
+                Log.d("넘어갈id",sID);
+                Log.d("넘어갈pw",sPW);
+
+                new LoginService(){
+                    @Override
+                    public void login() {
+
+                        if(mLogin.execute(sID,sPW)){
+                            Toast.makeText(context,"로그인 성공",Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(context,MemberList.class));
+                        }else{
+                            Toast.makeText(context,"로그인 실패",Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(context,MainActivity.class));
+                        }
+
+                    }
+                }.login();
+
+            }
+        });
+        findViewById(R.id.joinBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    //인터페이스에 메소드 하나만.
+    /*interface : 추상메소드만 있음.
+    abstract : concrete 메소드와 추상메소드 함께 있음.
+    */
+
+    public static interface ListService {public ArrayList<?> list();}
+
+    public static class mySQLiteHelper extends SQLiteOpenHelper{
+
+        public mySQLiteHelper(Context context) {
+            super(context, DB_NAME, null, 1);
+            this.getWritableDatabase();
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL(String.format("CREATE TABLE IF NOT EXISTS %s" +
+                            "(%s INTEGER PRIMARY KEY AUTOINCREMENT,%s TEXT,%s TEXT," +
+                            "%s TEXT,%s TEXT,%s TEXT,%s TEXT);",
+                    MEMBER_TABLE,MEMBER_SEQ,MEMBER_NAME,MEMBER_PASSWORD,MEMBER_EMAIL,MEMBER_PHONE,MEMBER_ADDRESS,MEMBER_PHOTO));
+
+            for(int i=1;i<10;i++){
+                db.execSQL(
+                        String.format(
+                                "INSERT INTO %s(%s,%s,%s,%s,%s,%s)" +
+                                        "VALUES('%s','%s','%s','%s','%s','%s');",
+                                MEMBER_TABLE,MEMBER_NAME,MEMBER_PASSWORD,MEMBER_EMAIL,MEMBER_PHONE,MEMBER_ADDRESS,MEMBER_PHOTO,
+                                "홍길동"+i,"1","hong"+i+"@test.com","010-1234-567"+i,"서울"+i,"profileimg"));
+            }
+            Log.d("########## 실행한 지점","onCreate");
+
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            db.execSQL("DROP TABLE IF EXISTS"+MEMBER_TABLE );
+            onCreate(db);
+        }
+    }
+
+
+    private abstract class LoginQuery extends  MainActivity.QueryFactory{
+        SQLiteOpenHelper helper;
+        public LoginQuery(Context context) {
+            super(context);
+            helper = new MainActivity.mySQLiteHelper(context);
+        }
+
+        //alt+insert : overid Methods
+        @Override
+        public SQLiteDatabase getDB() {
+            return helper.getReadableDatabase();
+        }
+    }
+
+    private class MemberLogin extends LoginQuery{
+
+        public MemberLogin(Context context) {
+            super(context);
+        }
+        public boolean execute(String seq,String password){
+            return super
+                    .getDB()
+                    .rawQuery(String.format("SELECT password FROM %s WHERE %s = '%s' AND %s = '%s'",
+                            MEMBER_TABLE,MEMBER_SEQ,seq,MEMBER_PASSWORD,password),null)
+                    .moveToNext();
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/example/hb2009/contacts0617/MemberList.java
@@ -0,0 +1,181 @@
+package com.example.hb2009.contacts0617;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import static com.example.hb2009.contacts0617.MainActivity.MEMBER_ADDRESS;
+import static com.example.hb2009.contacts0617.MainActivity.MEMBER_EMAIL;
+import static com.example.hb2009.contacts0617.MainActivity.MEMBER_NAME;
+import static com.example.hb2009.contacts0617.MainActivity.MEMBER_PASSWORD;
+import static com.example.hb2009.contacts0617.MainActivity.MEMBER_PHONE;
+import static com.example.hb2009.contacts0617.MainActivity.MEMBER_PHOTO;
+import static com.example.hb2009.contacts0617.MainActivity.MEMBER_SEQ;
+import static com.example.hb2009.contacts0617.MainActivity.MEMBER_TABLE;
+
+public class MemberList extends AppCompatActivity {
+    interface ListService{public  ArrayList<?> list();}
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.member_list);
+
+        final Context context=MemberList.this;
+      /* final MemberList memberList = new MemberList(context);*/
+
+        new ListService() {
+            @Override
+            public ArrayList<?> list() {
+                return null;
+            }
+        }.list();
+
+        ListView listView= (ListView) findViewById(R.id.listView);
+        listView.setAdapter(new MemberAdapter(context,list));
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                return false;
+            }
+        });
+    }
+    private abstract class ListQuery extends MainActivity.QueryFactory{
+        MainActivity.mySQLiteHelper helper;
+        public ListQuery(Context context) {
+            super(context);
+            helper = new MainActivity.mySQLiteHelper(context);
+        }
+
+        @Override
+        public SQLiteDatabase getDB() {
+            return helper.getReadableDatabase();
+        }
+    }
+
+
+    private class getList extends ListQuery{
+
+        public getList(Context context) {
+            super(context);
+        }
+
+
+        @Override
+        public SQLiteDatabase getDB() {
+            return super.getDB();
+        }
+
+        public ArrayList<MainActivity.Member> list(){
+            ArrayList<MainActivity.Member> list = new ArrayList<>();
+
+            Cursor cursor=this.getDB().rawQuery(String.format("SELECT * FROM %s ;",MEMBER_TABLE),null);
+
+            MainActivity.Member m=null;
+            if(cursor!=null){
+                if (cursor.moveToFirst()){
+                    do{
+                        m=new MainActivity.Member();
+                        m.seq=Integer.parseInt(cursor.getString(cursor.getColumnIndex(MEMBER_SEQ)));
+                        m.password=cursor.getString(cursor.getColumnIndex(MEMBER_PASSWORD));
+                        m.addr=cursor.getString(cursor.getColumnIndex(MEMBER_ADDRESS));
+                        m.email=cursor.getString(cursor.getColumnIndex(MEMBER_EMAIL));
+                        m.name=cursor.getString(cursor.getColumnIndex(MEMBER_NAME));
+                        m.phone=cursor.getString(cursor.getColumnIndex(MEMBER_PHONE));
+                        m.photo=cursor.getString(cursor.getColumnIndex(MEMBER_PHOTO));
+                        list.add(m);
+                    }while(cursor.moveToNext());
+
+                }
+            }else{
+                Log.d("등록된 회원이 ","없습니다.");
+            }
+
+            Log.d("###### 전체목록 :",list.get(0).toString());
+            return list;
+        }
+
+    }
+
+    private class MemberAdapter extends BaseAdapter {
+        ArrayList<MainActivity.Member>list;
+        LayoutInflater inflater;
+        public MemberAdapter(Context context, ArrayList<MainActivity.Member> list) {
+            this.list=list;
+            this.inflater= LayoutInflater.from(context);
+        }
+        private int[] photos={
+                R.drawable.cupcake,
+                R.drawable.donut,
+                R.drawable.eclair,
+                R.drawable.froyo,
+                R.drawable.gingerbread,
+                R.drawable.honeycomb,
+                R.drawable.icecream,
+                R.drawable.jellybean,
+                R.drawable.kitkat,
+                R.drawable.lollipop
+        };
+
+        @Override
+        public int getCount() {
+            return list.size();
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return list.get(i);
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return i;
+        }
+
+        @Override
+        public View getView(int i, View v, ViewGroup g) {
+            ViewHolder holder;
+            if(v==null){
+                v=inflater.inflate(R.layout.member_item,null);
+                holder=new ViewHolder();
+                holder.profileimg= (ImageView) v.findViewById(R.id.profileimg);
+                holder.name= (TextView) v.findViewById(R.id.name);
+                holder.phone= (TextView) v.findViewById(R.id.phone);
+                v.setTag(holder);
+            }else{
+                holder= (ViewHolder) v.getTag();
+            }
+            holder.profileimg.setImageResource(photos[i]);
+            holder.name.setText(list.get(i).name);
+            holder.phone.setText(list.get(i).phone);
+            return v;
+        }
+    }
+    static class ViewHolder{
+        ImageView profileimg;
+        TextView name;
+        TextView phone;
+    }
+
+
+}
