93aa5c886bbc5af03ea52755432fa2ac52273837
ariel720 <ariel720@naver.com>
2017-06-24 10:05:39 +0900

    create delete

--- app/src/main/java/com/example/hb2009/contacts0617/MemberList.java
+++ app/src/main/java/com/example/hb2009/contacts0617/MemberList.java
@@ -1,9 +1,11 @@
 package com.example.hb2009.contacts0617;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -60,8 +62,15 @@ public class MemberList extends AppCompatActivity {
         });
         listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
-            public boolean onItemLongClick(AdapterView<?> p, View v, int i, long l) {
-                return false;
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int i, long id) {
+                MainActivity.Member m = (MainActivity.Member) listView.getItemAtPosition(i);
+                new AlertDialog.Builder(context).setTitle("DELETE").setMessage("진짜류?").setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        
+                    }
+                }).show();
+                return true;
             }
         });
     }
@@ -180,6 +189,42 @@ public class MemberList extends AppCompatActivity {
         TextView name;
         TextView phone;
     }
+
+    //===============DELETE=====================
+    //1.main에서 만들었던 그 sqlite db를 가져오고 있음.
+    private abstract class DeleteQuery extends MainActivity.QueryFactory{
+        MainActivity.mySQLiteHelper helper;
+        public DeleteQuery(Context context) {
+            super(context);
+            helper = new MainActivity.mySQLiteHelper(context);
+        }
+
+        @Override
+        public SQLiteDatabase getDB() {
+            return helper.getWritableDatabase();
+        }
+    }
+
+    private class getDelete extends DeleteQuery{
+
+        public getDelete(Context context) {
+            super(context);
+        }
+
+        //DeleteQuery에서 만든 getWritableDatabase를 가져오기
+        @Override
+        public SQLiteDatabase getDB() {
+            return super.getDB();
+        }
+
+        public void execute(String seq){
+            String sql = String.format("DELETE FROM %s WHERE %s = '%s'",MEMBER_TABLE,MEMBER_SEQ,seq);
+            super.getDB().execSQL(sql);
+            super.getDB().close(); //삭제한후에 DB닫기.
+        }
+    }
+
+
 
 
 }
